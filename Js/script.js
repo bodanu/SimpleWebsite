0368caa4b0196849ce80b0c3bcbcf134fde51537
bogdan0hiriscau <ionncelrau@gmail.com>
2018-09-02 22:57:22 +0300

    Minor changes

--- Js/script.js
+++ Js/script.js
@@ -14,62 +14,65 @@
 //         modal.style.display = 'none';
 //       }
 //   }
-var modal = document.getElementById('myModal');
+var modal1 = document.getElementById('myModal');
 
 var btn = document.getElementById('two');
       
 var span = document.getElementsByClassName('close')[0];
       
 $(btn).click(function() {
-  $(modal).css("display", "block");
+  $(modal1).fadeIn(500);
 });
       
 $(span).click(function() {
-  $(modal).fadeOut(1000);
+  $(modal1).fadeOut(500);
 });
 
-window.onclick =   function(event) {
-         if (event.target == modal) {
-         $(modal).fadeOut(1000);
-       }
-   }
-
-   var modal = document.getElementById('myModal');
+// window.onclick =   function(event) {
+//          if (event.target == modal1) {
+//          $(modal1).fadeOut(1000);
+//        }
+//    }
+ 
 
 var btn = document.getElementById('three');
       
 var span = document.getElementsByClassName('close')[0];
       
 $(btn).click(function() {
-  $(modal).css("display", "block");
+  $(modal1).fadeIn(500);
 });
       
 $(span).click(function() {
-  $(modal).fadeOut(1000);
+  $(modal1).fadeOut(500);
 });
 
-window.onclick =   function(event) {
-         if (event.target == modal) {
-         $(modal).fadeOut(1000);
-       }
-   }
+// window.onclick =   function(event) {
+//   var modal = document.getElementById('myModal');
+//          if (event.target == modal) {
+//          $(modal).fadeOut(500);
+//        }
+//    }
 
-   var modal = document.getElementById('myModal');
-
+var modal2 = document.getElementById('myModal1'); 
 var btn = document.getElementById('four');
       
 var span = document.getElementsByClassName('close')[0];
       
 $(btn).click(function() {
-  $(modal).css("display", "block");
+  $("#myModal1").css("display", "block");
 });
-      
-$(span).click(function() {
-  $(modal).fadeOut(1000);
+ 
+$("#close").click(function() {
+  $("#myModal1").fadeOut(1000);
 });
 
 window.onclick =   function(event) {
-         if (event.target == modal) {
-         $(modal).fadeOut(1000);
+  
+         if (event.target == modal1) {
+         $(modal1).fadeOut(1000);
        }
+        if (event.target == modal2) {
+          $(modal2).fadeOut(500)
+        }
    }
